7314350ef91967a1f25615589cbcb604c8d4dda8
TouHouNo <czerwiiii@gmail.com>
2026-02-27 18:54:06 +0100

    fix: align homebrew formula to v0.1.1 release

--- Formula/ads-cli.rb
+++ Formula/ads-cli.rb
@@ -1,20 +1,20 @@
 class AdsCli < Formula
   desc "CLI for searching and working with Google Docs"
-  homepage "https://github.com/skraus/ads-cli"
-  version "0.1.0"
+  homepage "https://github.com/czerwix/ads-cli"
+  version "0.1.1"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/skraus/ads-cli/releases/download/v#{version}/ads-macos-arm64.tar.gz"
-      sha256 "1111111111111111111111111111111111111111111111111111111111111111" # TODO: Replace with real arm64 release checksum after publishing v#{version} assets.
+      url "https://github.com/czerwix/ads-cli/releases/download/v#{version}/ads-macos-arm64.tar.gz"
+      sha256 "c136d0829aad4c7451ba79ae4d692f500047b8dbba02a1b7040dce8ab5909fee"
     else
-      url "https://github.com/skraus/ads-cli/releases/download/v#{version}/ads-macos-x86_64.tar.gz"
-      sha256 "2222222222222222222222222222222222222222222222222222222222222222" # TODO: Replace with real x86_64 release checksum after publishing v#{version} assets.
+      url "https://github.com/czerwix/ads-cli/releases/download/v#{version}/ads-macos-x86_64.tar.gz"
+      sha256 "f87fc80d69ebe941e4f75a498d6890115aa3d02baf4d61ee5728841dc0436012"
     end
   end
 
-  head "https://github.com/skraus/ads-cli.git", branch: "main" do
+  head "https://github.com/czerwix/ads-cli.git", branch: "main" do
     depends_on "swift" => :build
   end
 
